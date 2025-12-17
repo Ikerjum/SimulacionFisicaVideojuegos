7280d918c65031b20f0bf79e9a1d4ff5e7e34b9c
Iker-kun <ikerjuma@ucm.es>
2025-12-18 00:54:40 +0100

    Comentarios

--- skeleton/EnemyManager.cpp
+++ skeleton/EnemyManager.cpp
@@ -4,11 +4,12 @@
 EnemyManager::EnemyManager(PxPhysics* gPhysics, PxScene* gScene, std::vector<ForceGenerator*> const& _forceGenerators) :
 	_punt(0), _lifes(3), _record(0)
 {
+	//VARIABLES DE LA UI
 	Puntuation = "PUNTUACION: " + std::to_string(_punt);
 	Lifes = "LIFES: " + std::to_string(_lifes);
 	Record = "RECORD: " + std::to_string(_record);
 
-	//new EnemyGenerator(Vector3(50.0f, 20.0f, -190.0f), Vector3(50.0f, 20.0f, 190.0f), gPhysics, gScene);
+	//GENERADORES DE ENEMIGOS
 	_enemyGenerators[0] = new EnemyGenerator(Vector3(50.0f, 20.0f, -190.0f), Vector3(50.0f, 20.0f, 190.0f), gPhysics, gScene);
 	for (auto& f : _forceGenerators) {
 		if (f->getType() == ForceGenerator::GRAVITY) _enemyGenerators[0]->addForceGenerator(f);
@@ -32,14 +33,17 @@ void EnemyManager::updateEnemyManager(double t)
 	for (int i = 0; i < _enemyGenerators.size(); ++i) {
 		_enemyGenerators[i]->updateEnemyGenerator(t);
 
-		if (_enemyGenerators[i]->getWinPoints()) {
+		if (_enemyGenerators[i]->getWinPoints()) { //EN CASO DE GANAR PUNTOS
+
 			_punt += 100;
 			Puntuation = "POINTS: " + std::to_string(_punt);
 			_enemyGenerators[i]->setWinPoints(false);
 			break;
+
 		}
 
-		if (_enemyGenerators[i]->getLoseLife()) {
+		if (_enemyGenerators[i]->getLoseLife()) { //EN CASO DE PERDER UNA VIDA (POSIBILIDAD DE DERROTA)
+			
 			if (_lifes > 0) {
 				_lifes--;
 			}
@@ -60,38 +64,7 @@ void EnemyManager::updateEnemyManager(double t)
 			Lifes = "LIFES: " + std::to_string(_lifes);
 			_enemyGenerators[i]->setLoseLife(false);
 			break;
+
 		}
 	}
-
-	//if (/*(*it)->getTimeOfLife() <= 0 || */(*it)->getActor()->getGlobalPose().p.y <= 0) {
-	//	delete (*it);
-	//	it = _enemies.erase(it);
-	//	_punt += 100;
-	//	Puntuation = "POINTS: " + std::to_string(_punt);
-	//}
-	//else if ((*it)->getActor()->getGlobalPose().p.z >= _posGoal.z) {
-
-	//	_lifes -= 1;
-
-	//	if (_lifes > 0) {
-	//		delete (*it);
-	//		it = _enemies.erase(it);
-	//		Lifes = "LIFES: " + std::to_string(_lifes);
-	//		break;
-	//	}
-	//	else {
-	//		if (_punt > _record) {
-	//			Record = "RECORD: " + std::to_string(_punt);
-	//		}
-	//		_punt = 0;
-	//		Puntuation = "POINTS: " + std::to_string(_punt);
-	//		_lifes = 3;
-	//		Lifes = "LIFES: " + std::to_string(_lifes);
-	//		resetEnemies = true;
-	//		break;
-	//	}
-	//}
-	//else {
-	//	++it;
-	//}
 }
